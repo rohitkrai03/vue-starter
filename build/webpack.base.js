b7b39315bb1dd46841826253f3a33e011640e520
Rohit Rai <rohitkrai03@gmail.com>
2017-08-10 15:03:58 +0530

    Building from basic webpack setup

---
+++ build/webpack.base.js
@@ -0,0 +1,12 @@
+const path = require('path');
+
+const config = {
+  entry: './src/main.js',
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    publicPath: '/dist/',
+    filename: 'build.js'
+  }
+};
+
+module.exports = config;
